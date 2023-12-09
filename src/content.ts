93c58849fb72b01e12f7c933ea349462e9850e3e
aoirint <aoirint@gmail.com>
2023-12-09 11:26:12 +0900

    remove closure

--- src/content.ts
+++ src/content.ts
@@ -1,140 +1,138 @@
-;(function () {
-  console.log('YouTube Metadata Extension Loading')
+console.log('YouTube Metadata Extension Loading')
 
-  const menuElement = document.createElement('div')
-  menuElement.style.backgroundColor = '#FFFFFFCC'
-  menuElement.style.padding = '4px 16px'
-  menuElement.style.fontSize = '12pt'
-  menuElement.style.display = 'flex'
-  menuElement.style.flexDirection = 'column'
-  menuElement.style.zIndex = '999'
-  menuElement.style.position = 'fixed'
-  menuElement.style.left = '0px'
-  menuElement.style.bottom = '0px'
+const menuElement = document.createElement('div')
+menuElement.style.backgroundColor = '#FFFFFFCC'
+menuElement.style.padding = '4px 16px'
+menuElement.style.fontSize = '12pt'
+menuElement.style.display = 'flex'
+menuElement.style.flexDirection = 'column'
+menuElement.style.zIndex = '999'
+menuElement.style.position = 'fixed'
+menuElement.style.left = '0px'
+menuElement.style.bottom = '0px'
 
-  let intervalVideoObjectId: number | null = null
-  let intervalChannelId: number | null = null
+let intervalVideoObjectId: number | null = null
+let intervalChannelId: number | null = null
 
-  const channelIdElement = document.createElement('div')
-  const videoIdElement = document.createElement('div')
-  const uploadDateElement = document.createElement('div')
-  const startTimeElement = document.createElement('div')
-  const endTimeElement = document.createElement('div')
+const channelIdElement = document.createElement('div')
+const videoIdElement = document.createElement('div')
+const uploadDateElement = document.createElement('div')
+const startTimeElement = document.createElement('div')
+const endTimeElement = document.createElement('div')
 
-  channelIdElement.innerText = 'チャンネルID'
-  videoIdElement.innerText = '動画ID'
-  uploadDateElement.innerText = 'アップロード日'
-  startTimeElement.innerText = '開始時間'
-  endTimeElement.innerText = '終了時間'
+channelIdElement.innerText = 'チャンネルID'
+videoIdElement.innerText = '動画ID'
+uploadDateElement.innerText = 'アップロード日'
+startTimeElement.innerText = '開始時間'
+endTimeElement.innerText = '終了時間'
 
-  const channelIdInputElement = document.createElement('input')
-  const videoIdInputElement = document.createElement('input')
-  const uploadDateInputElement = document.createElement('input')
-  const startTimeInputElement = document.createElement('input')
-  const endTimeInputElement = document.createElement('input')
+const channelIdInputElement = document.createElement('input')
+const videoIdInputElement = document.createElement('input')
+const uploadDateInputElement = document.createElement('input')
+const startTimeInputElement = document.createElement('input')
+const endTimeInputElement = document.createElement('input')
 
-  function clickToSelectAllHandler(event: MouseEvent) {
-    if (!(event.target instanceof HTMLInputElement)) {
-      throw Error('Unexpected event target type')
-    }
-
-    event.target.select()
+function clickToSelectAllHandler(event: MouseEvent) {
+  if (!(event.target instanceof HTMLInputElement)) {
+    throw Error('Unexpected event target type')
   }
 
-  channelIdInputElement.addEventListener('click', clickToSelectAllHandler)
-  videoIdInputElement.addEventListener('click', clickToSelectAllHandler)
-  uploadDateInputElement.addEventListener('click', clickToSelectAllHandler)
-  startTimeInputElement.addEventListener('click', clickToSelectAllHandler)
-  endTimeInputElement.addEventListener('click', clickToSelectAllHandler)
+  event.target.select()
+}
 
-  menuElement.appendChild(channelIdElement)
-  menuElement.appendChild(channelIdInputElement)
-  menuElement.appendChild(videoIdElement)
-  menuElement.appendChild(videoIdInputElement)
-  menuElement.appendChild(uploadDateElement)
-  menuElement.appendChild(uploadDateInputElement)
-  menuElement.appendChild(startTimeElement)
-  menuElement.appendChild(startTimeInputElement)
-  menuElement.appendChild(endTimeElement)
-  menuElement.appendChild(endTimeInputElement)
-  document.body.appendChild(menuElement)
+channelIdInputElement.addEventListener('click', clickToSelectAllHandler)
+videoIdInputElement.addEventListener('click', clickToSelectAllHandler)
+uploadDateInputElement.addEventListener('click', clickToSelectAllHandler)
+startTimeInputElement.addEventListener('click', clickToSelectAllHandler)
+endTimeInputElement.addEventListener('click', clickToSelectAllHandler)
 
-  function getVideoId() {
-    const currentUrl = new URL(location.href)
+menuElement.appendChild(channelIdElement)
+menuElement.appendChild(channelIdInputElement)
+menuElement.appendChild(videoIdElement)
+menuElement.appendChild(videoIdInputElement)
+menuElement.appendChild(uploadDateElement)
+menuElement.appendChild(uploadDateInputElement)
+menuElement.appendChild(startTimeElement)
+menuElement.appendChild(startTimeInputElement)
+menuElement.appendChild(endTimeElement)
+menuElement.appendChild(endTimeInputElement)
+document.body.appendChild(menuElement)
 
-    const paramVideoId = currentUrl.searchParams.get('v')
-    if (paramVideoId != null) {
-      return paramVideoId
-    }
+function getVideoId() {
+  const currentUrl = new URL(location.href)
 
-    const urlPath = currentUrl.pathname
-    const liveUrlMatch = urlPath.match(/^\/live\/(.+)$/)
-    if (liveUrlMatch != null) {
-      return liveUrlMatch[1]
-    }
-
-    return null
+  const paramVideoId = currentUrl.searchParams.get('v')
+  if (paramVideoId != null) {
+    return paramVideoId
   }
 
-  function getChannelId() {
-    const infoCardChannelAnchorElement = document.querySelector(
-      '#infocard-channel-button > ytd-button-renderer > yt-button-shape > a',
-    )
-    if (infoCardChannelAnchorElement != null) {
-      const channelAboutUrlString = infoCardChannelAnchorElement.getAttribute('href')
-      const channelAboutUrl = new URL(channelAboutUrlString, location.href)
-      const channelAboutUrlMatch = channelAboutUrl.pathname.match(/^\/channel\/(.+)\/about$/)
-
-      if (channelAboutUrlMatch != null) {
-        return channelAboutUrlMatch[1]
-      }
-    }
-
-    return null
+  const urlPath = currentUrl.pathname
+  const liveUrlMatch = urlPath.match(/^\/live\/(.+)$/)
+  if (liveUrlMatch != null) {
+    return liveUrlMatch[1]
   }
 
-  function doIntervalChannelId() {
-    const channelId = getChannelId()
-    if (channelId != null) {
-      channelIdInputElement.value = channelId
+  return null
+}
 
-      // stop interval if loaded
-      clearInterval(intervalChannelId)
+function getChannelId() {
+  const infoCardChannelAnchorElement = document.querySelector(
+    '#infocard-channel-button > ytd-button-renderer > yt-button-shape > a',
+  )
+  if (infoCardChannelAnchorElement != null) {
+    const channelAboutUrlString = infoCardChannelAnchorElement.getAttribute('href')
+    const channelAboutUrl = new URL(channelAboutUrlString, location.href)
+    const channelAboutUrlMatch = channelAboutUrl.pathname.match(/^\/channel\/(.+)\/about$/)
+
+    if (channelAboutUrlMatch != null) {
+      return channelAboutUrlMatch[1]
     }
   }
 
-  function doIntervalVideoObject() {
-    const videoObjectElement = document.querySelector('#watch7-content')
-    if (videoObjectElement == null) return
+  return null
+}
 
-    const uploadDateElement: HTMLMetaElement | null = videoObjectElement.querySelector(
-      'meta[itemprop="uploadDate"]',
-    )
-    const uploadDateString = uploadDateElement != null ? uploadDateElement.content : ''
-    uploadDateInputElement.value = uploadDateString
-
-    const publicationElement = videoObjectElement.querySelector('span[itemprop="publication"]')
-    if (publicationElement != null) {
-      const startDateElement: HTMLMetaElement | null = publicationElement.querySelector(
-        'meta[itemprop="startDate"]',
-      )
-      const startDateString = startDateElement != null ? startDateElement.content : ''
-      const endDateElement: HTMLMetaElement | null = publicationElement.querySelector(
-        'meta[itemprop="endDate"]',
-      )
-      const endDateString = endDateElement != null ? endDateElement.content : ''
-
-      startTimeInputElement.value = startDateString
-      endTimeInputElement.value = endDateString
-    }
+function doIntervalChannelId() {
+  const channelId = getChannelId()
+  if (channelId != null) {
+    channelIdInputElement.value = channelId
 
     // stop interval if loaded
-    clearInterval(intervalVideoObjectId)
+    clearInterval(intervalChannelId)
+  }
+}
+
+function doIntervalVideoObject() {
+  const videoObjectElement = document.querySelector('#watch7-content')
+  if (videoObjectElement == null) return
+
+  const uploadDateElement: HTMLMetaElement | null = videoObjectElement.querySelector(
+    'meta[itemprop="uploadDate"]',
+  )
+  const uploadDateString = uploadDateElement != null ? uploadDateElement.content : ''
+  uploadDateInputElement.value = uploadDateString
+
+  const publicationElement = videoObjectElement.querySelector('span[itemprop="publication"]')
+  if (publicationElement != null) {
+    const startDateElement: HTMLMetaElement | null = publicationElement.querySelector(
+      'meta[itemprop="startDate"]',
+    )
+    const startDateString = startDateElement != null ? startDateElement.content : ''
+    const endDateElement: HTMLMetaElement | null = publicationElement.querySelector(
+      'meta[itemprop="endDate"]',
+    )
+    const endDateString = endDateElement != null ? endDateElement.content : ''
+
+    startTimeInputElement.value = startDateString
+    endTimeInputElement.value = endDateString
   }
 
-  const videoId = getVideoId()
-  videoIdInputElement.value = videoId != null ? videoId : null
+  // stop interval if loaded
+  clearInterval(intervalVideoObjectId)
+}
 
-  intervalChannelId = setInterval(doIntervalChannelId, 100)
-  intervalVideoObjectId = setInterval(doIntervalVideoObject, 100)
-})()
+const videoId = getVideoId()
+videoIdInputElement.value = videoId != null ? videoId : null
+
+intervalChannelId = setInterval(doIntervalChannelId, 100)
+intervalVideoObjectId = setInterval(doIntervalVideoObject, 100)
