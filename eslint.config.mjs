ddf1130d8f67c12dfb5b70f940ee2e5915ee1fdc
aoirint <aoirint@gmail.com>
2025-11-16 11:55:38 +0900

    chore(eslint): FlatCompatへの依存を削除

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,47 +1,31 @@
-import { defineConfig } from 'eslint/config'
 import typescriptEslint from '@typescript-eslint/eslint-plugin'
 import globals from 'globals'
 import tsParser from '@typescript-eslint/parser'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-})
-
-export default defineConfig([
+export default [
   {
     ignores: ['pnpm-lock.yaml'],
   },
+  js.configs.recommended,
   {
-    extends: compat.extends(
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier',
-    ),
-
+    files: ['**/*.ts'],
     plugins: {
       '@typescript-eslint': typescriptEslint,
     },
-
     languageOptions: {
       globals: {
         ...globals.browser,
       },
-
       parser: tsParser,
-      ecmaVersion: 5,
+      ecmaVersion: 2022,
       sourceType: 'module',
-
       parserOptions: {
         project: './tsconfig.json',
       },
     },
+    rules: {
+      ...typescriptEslint.configs.recommended.rules,
+    },
   },
-])
+]
